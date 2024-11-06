edb6bbfb1baf664fc41d6fcd9ea77c9f6e9e3f68
LayGomez <laylamelanygomez@gmail.com>
2024-11-06 15:02:00 +0100

    package herencia

---
+++ src/main/java/org/example/herencia/Main.java
@@ -0,0 +1,14 @@
+package org.example.herencia;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola");
+
+        // una clase race ->
+        // uqe al crearla cree un peloton de vehiculo
+        //publi void start() -> todos los vehiculos del peloton aceleren a la vez
+        Race peloton = new Race();
+        peloton.start();
+    }
+
+}
